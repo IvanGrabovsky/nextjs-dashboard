d49979ea080a54d28df554614c0f4ef056dba0b5
IvanGrabovsky <ivan.grabovsky.ua@gmail.com>
2024-01-09 23:23:42 +0200

    upload project

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Poppins } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+ 
+// export const inter = DotGothic16({ subsets: ['latin'], weight: ['400'] });
+export const lusitana = Lusitana({ weight: ['400', '700'], subsets: ['latin'] });
+// import { Poppins, Inter } from 'next/font/google';
+//  
+export const inter = Poppins({ subsets: ['latin'], weight: ['400'] });
+// export const inter = Inter({ subsets: ['latin'] });
